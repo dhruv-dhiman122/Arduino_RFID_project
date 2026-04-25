9a4dfb8fda2a1fe6cdec825c7d64135c2dde5e92
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-04-25 09:58:37 +0530

    Added the configuration for pins

--- main.ino
+++ main.ino
@@ -1,7 +1,16 @@
 
+#define RSI_PIN 9
+#define SDA_PIN 10
+#define MOSI_PIN 11
+#define MISO_PIN 12
+#define SCK_PIN 13
 
 void setup() {
-
+    pinMode(MOSI_PIN, OUTPUT);
+    pinMode(MISO_PIN, INPUT);
+    pinMode(SCK_PIN, OUTPUT);
+    pinMode(SDA_PIN, OUTPUT);
+    pinMode(RSI_PIN, INPUT);
 }
 
 void loop() {
